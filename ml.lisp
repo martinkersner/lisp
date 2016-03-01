09b418a5aea74d83a4619ca0ef6695659b453ece
Martin Kersner <m.kersner@gmail.com>
2016-03-01 13:19:13 +0900

    simple neuron with sigmoid activation function

--- ml.lisp
+++ ml.lisp
@@ -4,3 +4,11 @@
 ; sigmoid function for a single value
 (defun sigmoid (n)
   (/ 1 (+ 1 (exp (* (- n))))))
+
+; simple model of neuron with sigmoid activation function
+; (neuron '(1 2) '(3 4) 2)
+(defun neuron (x w b)
+  (setf output (sigmoid (apply '+ (mapcar #'* (cons 1 x) (cons b w)))))
+  (if (> output 0)
+    1
+    0))
