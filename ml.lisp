5ac1c702614285846d59b2611a645979934eaefa
Martin Keršner <m.kersner@gmail.com>
2016-03-02 16:38:52 +0900

    cross-entropy

--- ml.lisp
+++ ml.lisp
@@ -12,3 +12,14 @@
   (if (> output 0)
     1
     0))
+
+; log of all elements in a list
+(defun log_list (x)
+  (mapcar #'log x))
+
+; cross-entropy
+; C = -1/n sum[y*ln(a) + (1-y)*ln(1-a)]
+(defun cross-entropy (a y)
+  (let ((one_minus_a (mapcar #'1- a))
+        (one_minus_y (mapcar #'1- y)))
+  (- (/ 1 (apply '+ (mapcar #'+ (mapcar #'* y (log_list a)) (mapcar #'* one_minus_y (log_list one_minus_a))))))))
