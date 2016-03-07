e85c0d280dfc2c5bd43d0ab483902adaa9dace42
Martin Kersner <m.kersner@gmail.com>
2016-03-07 21:34:42 +0900

    showing objects in given locations

--- wizards-adventure-game.lisp
+++ wizards-adventure-game.lisp
@@ -20,3 +20,15 @@
 
 (defun describe-paths (location edges)
   (apply #'append (mapcar #'describe-path (cdr (assoc location edges)))))
+
+(defparameter *objects* '(whiskey bucket frog chain))
+
+(defparameter *object-locations* '((whiskey living-room)
+                                   (bucket living-room)
+                                   (chain garden)
+                                   (frog garden)))
+
+(defun objects-at (loc objs obj-locs)
+  (labels ((at-loc-p (obj)
+    (eq (cadr (assoc obj obj-locs)) loc)))
+    (remove-if-not #'at-loc-p objs)))
