04144571c8af29262151bb5256a559504ad74a65
Martin Kersner <m.kersner@gmail.com>
2016-03-20 13:53:07 +0900

    modified eval function for a game

--- wizards-adventure-game.lisp
+++ wizards-adventure-game.lisp
@@ -74,3 +74,10 @@
     (flet ((quote-it (x)
             (list 'quote x)))
       (cons (car cmd) (mapcar #'quote-it (cdr cmd))))))
+
+(defparameter *allowed-commands* '(look walk pickup inventory))
+
+(defun game-eval (sexp)
+  (if (member (car sexp) *allowed-commands*)
+      (eval sexp)
+      '(i do not know that command)))
