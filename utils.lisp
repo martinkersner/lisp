5542b544885422c50d7efbfcb33f9bae962b5a80
Martin Kersner <m.kersner@gmail.com>
2016-03-20 12:51:20 +0900

    greeting function

--- utils.lisp
+++ utils.lisp
@@ -2,3 +2,9 @@
   (if list
     (1+ (my-list-length (cdr list)))
     0))
+
+(defun say-hello ()
+  (print "Please write your name:")
+  (let ((name (read)))
+       (print "Nice to meet you,")
+       (prin1 name)))
